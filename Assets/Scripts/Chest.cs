983cded7b32b0bf56361920579f3eb5051e286b6
sugihaf <sugihaf@gmail.com>
2021-11-22 15:23:53 +0700

    add update camera movement and interactable object

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    public Sprite emptyChest;
+    public int moneyAmount = 5;
+
+    protected override void OnCollect()
+    {
+        if (!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChest;
+            Debug.Log("Get "+moneyAmount+" Money");
+        }
+        
+    }
+}
